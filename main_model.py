e15f7a986b38e0ce2042f790fa69dac15eb28e67
Imruj <nongkhran04611@gmail.com>
2025-07-10 16:01:19 +0700

    Add files via upload

--- main_model.py
+++ main_model.py
@@ -1,1 +1,11 @@
-print("hello world")
+import os
+#import librosa as ls
+
+dataset_path = "snoring_sound_dataset"
+files = os.listdir(dataset_path)
+print("Files in dataset:", files)
+
+# for file in files:
+#     if file.endswith('.wav'):
+#         audio, sr = librosa.load(os.path.join(dataset_path, file), sr=None)
+#         print(f"Loaded {file} with sample rate {sr}")
